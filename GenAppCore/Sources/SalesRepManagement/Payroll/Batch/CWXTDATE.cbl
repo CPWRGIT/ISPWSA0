36d15d6222c97adacadd18b9b58ceba85622431c
stupot1966 <stuart_ashby@bmc.com>
2021-07-26 10:24:19 +0000

    Validate IWG381A

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTDATE.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTDATE.cbl
@@ -8,7 +8,7 @@
       *  FOR XPEDITER/TSO TRAINING SESSIONS                            *
       *                                                                *
       ******************************************************************
-      * 20.07.01 CWCC with IWG279A
+      * 20.07.01 CWCC with IWG381A & TAWS
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
