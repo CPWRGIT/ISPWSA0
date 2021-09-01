4b79bf07189aee4c610b019c6ea68e7d39bf1baf
stupot1966 <stuart_ashby@bmc.com>
2021-09-01 07:16:09 +0100

    Westpac demo

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
@@ -18,7 +18,7 @@
       ******************************************************************
       * 20.06.02 Workbench
       * Latest pipeline
-      *VSC
+      * Westpac demo
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
