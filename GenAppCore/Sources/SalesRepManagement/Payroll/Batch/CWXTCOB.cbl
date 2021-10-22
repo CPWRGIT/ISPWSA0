0f676004a161476cdafa5e14cb18601e3f9e183e
stupot1966 <stuart_ashby@bmc.com>
2021-10-22 19:21:32 +0100

    Take 2

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
@@ -16,7 +16,7 @@
       *                                                                *
       *                                                                *
       ******************************************************************
-      * Tony
+      * Tony take 2
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
