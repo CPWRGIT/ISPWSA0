da93efd77a1a0fec3bb5f30bcc9dea2c44af9cec
stupot1966 <stuart_ashby@bmc.com>
2022-11-10 18:47:55 +0000

    See if checkout fixes the problem

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
@@ -17,6 +17,7 @@
       *                                                                *
       ******************************************************************
       * TAWS 20.10.01 with password change on HCI
+      * Test the latest CLI with a freshly cloned repo
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
